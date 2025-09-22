8df5e1dd7e102f7ab9c053c1a458216e442f891c
Vida Quinia <quiniavidamarie@gmail.com>
2025-09-22 22:13:21 +0800

    added design to the register form

--- register.js
+++ register.js
@@ -10,11 +10,18 @@ function movePanel(){
     const signUpForm = document.querySelector('.sign-up')
     const signInForm = document.querySelector('.sign-in')
 
+    const signUpPanel = document.querySelector('.sign-up-panel')
+    const signInPanel = document.querySelector('.sign-in-panel')
+
     moveToSignIn.addEventListener('click', () => {
         panel.classList.add('change-panel')
+        signInPanel.classList.add('move-left')
+        signUpPanel.classList.remove('move-right')
     })
 
     moveToSignUp.addEventListener('click', () => {
         panel.classList.remove('change-panel')
+        signInPanel.classList.remove('move-left')
+        signUpPanel.classList.add('move-right')
     })
 }
